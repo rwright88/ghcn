63d725b8a27a65d2252dd4bf411b7a210d103ea8
Ray Wright <raywright88@gmail.com>
2018-12-13 19:52:27 -0500

    Clean up

--- other/testing.R
+++ other/testing.R
@@ -32,11 +32,9 @@ bench_createdb <- function(dir_files, file_db) {
   res / n_files
 }
 
-# https://stackoverflow.com/questions/4114940/select-random-rows-in-sqlite
-query_rand <- function(file_db, n_rows = 1e3) {
+query_rand <- function(file_db, n_rows) {
   con <- dbConnect(SQLite(), file_db)
   query <- paste0("SELECT * FROM dly_core ORDER BY RANDOM() LIMIT ", n_rows, ";")
-  # query <- paste0("SELECT * FROM dly_core WHERE id IN (SELECT id FROM dly_core ORDER BY RANDOM() LIMIT ", n_rows, ");")
   res <- dbSendQuery(con, query)
   dat <- dbFetch(res)
   dbClearResult(res)
@@ -47,25 +45,9 @@ query_rand <- function(file_db, n_rows = 1e3) {
 
 # run ---------------------------------------------------------------------
 
-# n <- 100
-# files <- list.files(dir_files, full.names = TRUE)
-# files <- files[sample(n)]
-#
-# system.time(dly1 <- f1(files)) / n
-# system.time(dly2 <- f2(files)) / n
-#
-# all.equal(
-#   arrange(dly1, date),
-#   arrange(dly2, date)
-# )
-#
-# summary(dly1)
-#
-# lobstr::obj_size(dly1) / n
-
 # res <- bench_createdb(dir_files, file_db)
 # res
 
 system.time(
-  dat <- query_rand(file_db)
+  dat <- query_rand(file_db, n_rows = 100)
 )
